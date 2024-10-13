41f600e1ebb4e8e411a6c920f9fb46ed7280b601
h8m0ss <danarmenta32@gmail.com>
2024-10-13 12:18:10 -0600

    Formatting

--- lua/plugins/custom_plugins.lua
+++ lua/plugins/custom_plugins.lua
@@ -1,25 +1,28 @@
 return {
   {
-    "evanleck/vim-svelte", lazy = false
+    "evanleck/vim-svelte",
+    lazy = false,
   },
   {
-    "pangloss/vim-javascript", lazy = false
+    "pangloss/vim-javascript",
+    lazy = false,
   },
   {
-    "HerringtonDarkholme/yats.vim", lazy = false
+    "HerringtonDarkholme/yats.vim",
+    lazy = false,
   },
   {
-    "dhruvasagar/vim-table-mode", lazy = false
+    "dhruvasagar/vim-table-mode",
+    lazy = false,
   },
   {
-      "kylechui/nvim-surround",
-      version = "*", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
-      config = function()
-          require("nvim-surround").setup({
-             -- Configuration here, or leave empty to use defaults
-          })
-      end
-  }
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }
-
