eb1deec1e1d5cca43b0b63a8162999ef7918f3f3
h8m0ss <danarmenta32@gmail.com>
2024-11-24 17:59:11 -0600

    Added auto-session plugin

--- lua/plugins/custom_plugins.lua
+++ lua/plugins/custom_plugins.lua
@@ -25,4 +25,15 @@ return {
       }
     end,
   },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      -- log_level = 'debug',
+    },
+  },
 }
